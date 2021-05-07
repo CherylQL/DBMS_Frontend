c7ca2c690f1c9d686671fc1aa0c37156818e7765
Cheryl <1030821728@qq.com>
2021-05-07 19:37:48 +0800

    ✨初始版本

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Home from './routes/home.jsx';
+import Auth from './routes/auth.jsx';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component = {Home} />
+            <Route path="/auth" component={Auth} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { PageHeader,  Menu, message } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
+import { BrowserRouter as   Router } from 'react-router-dom';
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage:"book"
+    }
+    console.log("props",this.props.props)
+  }
+
+  handleClick = (item) => {
+    this.setState({
+      currentPage:item.key
+    },()=>{
+      const token = localStorage.getItem('token');
+      if(token === null){
+        message.error("没有权限~")
+      }else{
+        if(item.key === "book")
+          this.props.props.history.push('/')
+        else if(item.key === "record")
+          this.props.props.history.push('/auth')
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <PageHeader
+          className = "home_header"
+          title="简易数据库系统"
+          subTitle = "A Simple DBMS built by React & NodeJS"
+        />
+        <Router>
+          <Menu onClick={this.handleClick}  mode="horizontal">
+            <Menu.Item key = "book" icon={<MailOutlined />}>Book List</Menu.Item>
+            <Menu.Item key = "record" icon={<MailOutlined />}>Send Record List</Menu.Item>
+          </Menu>
+        </Router>
+      </div>
+    )
+  }
+}
+export default Header;
